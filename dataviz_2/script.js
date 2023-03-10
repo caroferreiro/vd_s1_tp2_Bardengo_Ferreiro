32b79581dde994f5e78604596612b7c6e29a96a9
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-03-10 13:13:15 -0300

    noveno

--- dataviz_2/script.js
+++ dataviz_2/script.js
@@ -1,27 +1,20 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
-    let chart = Plot.plot({
-      color: {scheme: 'spectral'},
-      marks: [
-        Plot.barY(data, {
-          x: 'anio_mision',
-          y: 'mision_hs',
-          fill: 'nacionalidad',
-          color: {scheme: 'BuRd'},
-          opacity: 0.7,
-          sort: 'nacionalidad',
-          title: (d) =>
-          `${d.nacionalidad}
-          Horas de misión: ${(d.mision_hs/24).toFixed(2)} días`,
-        //  Horas de misión: ${Math.round(d.mision_hs/24)} días`,
-        }),
-      ],
-      x: {
-        tickFormat: 'd',
-      },
-      marginLeft: 70,
-      height: 700,
-      width: 500,
-    })
-    d3.select('#chart').append(() => chart)
+  let chart = Plot.plot({
+    marks: [
+      Plot.line(data, Plot.groupY({y:'nacionalidad'}, {x:'anio_mision'}), {
+        //x: 'anio_mision',
+        //y: 'eva_mision_hs',
+        //z: 'nacionalidad',
+
+        stroke: 'nacionalidad',
+        strokeWidth: 2,
+        opacity: 0.5,
+        tension: 'natural',
+  }),
+    ],
+    x: {
+      tickFormat: 'd',
+    },
   })
-  
+  d3.select('#chart').append(() => chart)
+})
