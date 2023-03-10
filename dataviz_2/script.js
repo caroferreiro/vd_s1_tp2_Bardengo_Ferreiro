bca0843566a6cd6a9c07c5b4e3ccef0fcabb7ff6
santinabardengo <santibardengo@gmail.com>
2023-03-10 15:18:10 -0300

    santi2

--- dataviz_2/script.js
+++ dataviz_2/script.js
@@ -1,19 +1,28 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
+  //let dataABC = data.filter(
+    //d => d.nacionalidad == 'Canada' || d.nacionalidad == 'Alemania' 
+  //)
   let chart = Plot.plot({
     marks: [
-      Plot.line(data, Plot.groupY({y:'nacionalidad'}, {x:'anio_mision'}), {
-        //x: 'anio_mision',
-        //y: 'eva_mision_hs',
-        //z: 'nacionalidad',
-
-        stroke: 'nacionalidad',
-        strokeWidth: 2,
-        opacity: 0.5,
-        tension: 'natural',
-  }),
+      Plot.areaY(dataABC, {
+        x: 'anio_mision',
+        y: 'mision_hs',
+        // https://observablehq.com/@ee2dev/sorting-with-plot-a-collection-of-plot-examples
+        //sort: d => -d.pop, // orden descendente
+        fill: 'ocupacion',
+      }),
     ],
     x: {
       tickFormat: 'd',
+    },
+    y: {
+      tickFormat: d3.format(',.0f'),
+      grid: true,
+    },
+    marginLeft: 70,
+    line: true,
+    color: {
+      legend: true,
     },
   })
   d3.select('#chart').append(() => chart)
