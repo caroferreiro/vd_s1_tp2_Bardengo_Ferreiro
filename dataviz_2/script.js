9d5f2b2e936b53bfc74a3f0f1cc52c12e6fd31b9
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-03-12 20:45:24 -0300

    ahora?

--- dataviz_2/script.js
+++ dataviz_2/script.js
@@ -1,7 +1,4 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
-  //let dataABC = data.filter(
-    //d => d.nacionalidad == 'Canada' || d.nacionalidad == 'Alemania' 
-  //)
   let chart = Plot.plot({
     color: {scheme: 'set2'},
       marks: [
@@ -41,15 +38,6 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
     axisX: {
       valueFormatString: "#",
       interval: 1,
-    },
-    y: {
-      tickFormat: d3.format(',.0f'),
-      grid: true,
-    },
-    marginLeft: 70,
-    line: true,
-    color: {
-      legend: true,
     },
   })
   d3.select('#chart').append(() => chart)
