a4eff54830a6eb96f85ff0ba332813932485bda4
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-03-15 10:40:22 -0300

    loy

--- dataviz_4/script.js
+++ dataviz_4/script.js
@@ -1,16 +1,16 @@
 let chart
 d3.csv('astronautas.csv', d3.autoType).then(data => {
   chart = Plot.plot({
-    color: {scheme: 'accent'},
     marks: [
       Plot.dot(data, {
         x: 'anio_mision',
         y: 'mision_hs',
-        fill: 'genero',
+        //stroke: 'eva_mision_hs',
+        fill: 'eva_mision_hs',
         fillOpacity: 0.6,
         r: 'eva_mision_hs',
         title: (d) => `${d.nombre}
-        País: ${d.nacionalidad}`
+        País: ${d.nacionalidad}`,
       }),
       Plot.frame(),
     ],
@@ -18,19 +18,31 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
     nice: true,
     width: 700,
     height: 300,
+    marginBottom: 100,
+    marginTop: 30,
     r: { range: [0, 11] },
     facet: {
       data: data,
       x: 'genero',
+      label: null
     },
     x: { 
         ticks: 5,
         tickFormat: 'd',
         domain: [2009, 2020],
+        label: 'Año de Misión',
+        labelOffset: 40,
       },
     y: {
       ticks: 4,
-    }
+      label: 'Horas de misión',
+      labelOffset: 40,
+    },
+    color: {
+      //scheme: 'purples',
+      range: ['lemonchiffon', 'limegreen'],
+      legend: true,
+    },
   })
   d3.select('#chart').append(() => chart)
 })
