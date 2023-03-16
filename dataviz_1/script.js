2868c473075ac39fe816c74c7f1caf0d1eae1851
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-03-16 14:40:32 -0300

    ak

--- dataviz_1/script.js
+++ dataviz_1/script.js
@@ -2,19 +2,22 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
     let chart = Plot.plot({
       color: {scheme: 'spectral'},
       marks: [
-        Plot.axisY({anchor: "Left", label: "Misión Horas"}),
-        Plot.barY(data, Plot.groupX({y: 'sum'}, {
+        Plot.axisY({ anchor: "Left", label: "Misión Horas" }),
+        Plot.barY(data, 
+          //Plot.groupX({y: 'sum'},
+          {
           x: 'anio_mision', 
           y: 'mision_hs', 
           fill: 'nacionalidad',
-          color: {scheme: 'BuRd'},
           opacity: 0.7,
           sort: 'nacionalidad',
           title: (d) =>
           `${d.nacionalidad}
-          Días de misión: ${(d.mision_hs/24).toFixed(2)} días`,
-       // //  Horas de misión: ${Math.round(d.mision_hs/24)} días`,
-       })),
+          Horas de misión: ${(d.mision_hs).toFixed(2)} horas`,
+       // Horas de misión: ${Math.round(d.mision_hs/24)} días`,
+       },
+       ),
+       //),
       ],
       x: {
         tickFormat: 'd',
