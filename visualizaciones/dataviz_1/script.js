30873cd5d40ca04140fadefcdab32295678b6851
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-03-29 09:08:11 -0300

    y

--- visualizaciones/dataviz_1/script.js
+++ visualizaciones/dataviz_1/script.js
@@ -1,5 +1,6 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
     let datos_2016 = data.filter(d => d.anio_mision == '2016')
+    let datos_2019 = data.filter(d => d.anio_mision == '2019')
     let chart = Plot.plot({
       color: {
         scheme: 'spectral',
@@ -30,16 +31,43 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
           `${d.nacionalidad}`
           //Horas de misión: ${(d.mision_hs).toFixed(2)} horas`,
        // Horas de misión: ${Math.round(d.mision_hs/24)} días`,
-          }),
-          ),
-        // Plot.text(data, Plot.binX({y:'sum'}, {
-        //   x: 'anio_mision',
-        //   //y: 'mision_hs',
-        //   text: 'mision_hs',
-        //   fill: '#000000',
-        //   fontWeight: 'light',
-        //   fontSize: 12,
-        //   //dy: -20,
+          })),
+          Plot.barY(datos_2016, 
+            Plot.groupX({y: 'sum'}, {
+              x: 'anio_mision', 
+              y: 'mision_hs', 
+              fill: 'nacionalidad',
+              fillOpacity: 0.7,
+              stroke: 'nacionalidad',
+              strokeOpacity: 0.1,
+              strokeWeight: 0.1,
+              sort: 'nacionalidad',
+              title: (d) =>
+            `${d.nacionalidad}`
+          })),
+          Plot.barY(datos_2019, 
+            Plot.groupX({y: 'sum'}, {
+              x: 'anio_mision', 
+              y: 'mision_hs', 
+              fill: 'nacionalidad',
+              fillOpacity: 0.7,
+              stroke: 'nacionalidad',
+              strokeOpacity: 0.1,
+              strokeWeight: 0.1,
+              sort: 'nacionalidad',
+              title: (d) =>
+            `${d.nacionalidad}`
+          })),
+        //   Plot.text(data, Plot.binX({y:'sum'}, {
+        //     x: 'anio_mision',
+        //     //y: 'mision_hs',
+        //     filter: (d) => d.anio_mision == '2016',
+        //     // text: 'mision_hs',
+        //     fill: '#111111',
+        //     fontSize: 12,
+        //     fontWeight: 'bold',
+        //     dy: -360,
+        //     dx: -43,
         // })),
       ],
       x: {
