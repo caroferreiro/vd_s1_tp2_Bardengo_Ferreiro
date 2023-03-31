9553109eeeeb1eba4e29456e34d7397fb04c2fda
santinabardengo <santibardengo@gmail.com>
2023-03-31 08:00:56 -0300

    santu

--- visualizaciones/dataviz_1/script.js
+++ visualizaciones/dataviz_1/script.js
@@ -1,14 +1,16 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
-    let datos_paises = data.filter(d => d.nacionalidad == 'EE.UU.' || d.nacionalidad == 'U.S.S.R/Rusia' || d.nacionalidad == 'Japon' || d.nacionalidad == 'Italia' || d.nacionalidad == 'China' || d.nacionalidad == 'Canada' || d.nacionalidad == 'Alemania' || d.nacionalidad == 'Reino Unido' || d.nacionalidad == 'Paises Bajos')
-    let datos_2016 = datos_paises.filter(d => d.anio_mision == '2016')
-    let datos_2019 = datos_paises.filter(d => d.anio_mision == '2019')
+    let datos_paises = data.filter(d => d.nacionalidad == 'EE.UU.' || d.nacionalidad == 'U.S.S.R/Rusia') 
+    // || d.nacionalidad == 'Japon' || d.nacionalidad == 'Italia' || d.nacionalidad == 'China' || d.nacionalidad == 'Canada' || d.nacionalidad == 'Alemania' || d.nacionalidad == 'Reino Unido' || d.nacionalidad == 'Paises Bajos')
+    let datos_2016 = data.filter(d => d.anio_mision == '2016')
+    let datos_2019 = data.filter(d => d.anio_mision == '2019')
     let chart = Plot.plot({
       color: {
-        scheme: 'spectral',
         //range: ['#9e0142', '#d53e4f', '#a454b0', '#4daf4a', '#fee08b', '#e6f598', '#abdda4', 'lemonchiffon', 'yellowgreen', '#4daf4a', '#71d1b3'],
         //       Alemania    Canada     Rusia    Dinamarca    EE.UU.               Francia        Italia          Japon     Kazajistan
-        //range: ['#FF69B4', '#87cefa', '#7b68ee', '#98fb98', '#f08080', '#e6f598', '#abdda4', '#66c2a5', '#3288bd', '#5e4fa2'], '#bc80bd','#ffed6f'
-        legend: true,
+        //range: ['#FF69B4', '#87cefa', '#7b68ee', '#98fb98', '#f08080', '#e6f598', '#abdda4', '#66c2a5', '#3288bd', '#5e4fa2'], 
+        //range: d => (datos_paises.nacionalidad == 'EE.UU.' ? '#87cefa' : '#A9A9A9' && datos_paises.nacionalidad == 'U.S.S.R/Rusia' ? '#9e0142' : '#A9A9A9'), 
+        //scheme: d => (d.nacionalidad == 'EE.UU.' ? '#87cefa' : '#A9A9A9' && d.nacionalidad == 'U.S.S.R/Rusia' ? '#9e0142' : '	#A9A9A9'),
+        //legend:true
       },
       marks: [
         Plot.axisY({ 
@@ -19,15 +21,17 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
           labelOffset: 45,
           fontSize: 12,
         }),
-        Plot.barY(datos_paises, 
+        Plot.barY(data, 
           Plot.groupX({y: 'sum'}, {
             x: 'anio_mision', 
             y: 'mision_hs', 
-            fill: 'nacionalidad',
+            //fill: 'nacionalidad',
+            fill: d => (d.nacionalidad == 'EE.UU.' ? '#87cefa' : '#A9A9A9' && d.nacionalidad == 'U.S.S.R/Rusia' ? '#9e0142' : '	#A9A9A9'), 
             fillOpacity: 0.5,
-            stroke: 'nacionalidad',
+            stroke: d => (d.nacionalidad == 'EE.UU.' ? '#87cefa' : '#A9A9A9' && d.nacionalidad == 'U.S.S.R/Rusia' ? '#9e0142' : '	#A9A9A9'),
             strokeOpacity: 0.1,
             strokeWeight: 0.1,
+            legend: true,
             sort: 'nacionalidad',
             title: (d) =>
           `${d.nacionalidad}`
@@ -38,9 +42,9 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
             Plot.groupX({y: 'sum'}, {
               x: 'anio_mision', 
               y: 'mision_hs', 
-              fill: 'nacionalidad',
+              fill:d => (d.nacionalidad == 'EE.UU.' ? '#87cefa' : '#A9A9A9' && d.nacionalidad == 'U.S.S.R/Rusia' ? '#9e0142' : '	#A9A9A9'),
               fillOpacity: 0.8,
-              stroke: 'nacionalidad',
+              stroke: d => (d.nacionalidad == 'EE.UU.' ? '#87cefa' : '#A9A9A9' && d.nacionalidad == 'U.S.S.R/Rusia' ? '#9e0142' : '	#A9A9A9'),
               strokeOpacity: 0.1,
               strokeWeight: 0.1,
               sort: 'nacionalidad',
@@ -51,26 +55,53 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
             Plot.groupX({y: 'sum'}, {
               x: 'anio_mision', 
               y: 'mision_hs', 
-              fill: 'nacionalidad',
+              fill:d => (d.nacionalidad == 'EE.UU.' ? '#87cefa' : '#A9A9A9' && d.nacionalidad == 'U.S.S.R/Rusia' ? '#9e0142' : '	#A9A9A9'),
               fillOpacity: 0.7,
-              stroke: 'nacionalidad',
+              stroke:d => (d.nacionalidad == 'EE.UU.' ? '#87cefa' : '#A9A9A9' && d.nacionalidad == 'U.S.S.R/Rusia' ? '#9e0142' : '	#A9A9A9'),
               strokeOpacity: 0.1,
               strokeWeight: 0.1,
               sort: 'nacionalidad',
               title: (d) =>
             `${d.nacionalidad}`
           })),
-        //   Plot.text(data, Plot.binX({y:'sum'}, {
-        //     x: 'anio_mision',
-        //     //y: 'mision_hs',
-        //     filter: (d) => d.anio_mision == '2016',
-        //     // text: 'mision_hs',
-        //     fill: '#111111',
-        //     fontSize: 12,
-        //     fontWeight: 'bold',
-        //     dy: -360,
-        //     dx: -43,
-        // })),
+        Plot.text(
+          datos_2016,
+          Plot.groupX(
+          { y: 'sum' },
+          {
+          x: 'anio_mision',
+          y: 'mision_hs',
+          fill: 'black',
+          sort: 'nacionalidad',
+          fontWeight: 'bold',
+          fontSize: 12,
+          text: d => {
+          // d: es el grupo de astronautas de todos los países
+          console.log(d)
+          let sumaHs = d3.sum(d, d2 => d2.mision_hs)
+          return d3.format('d')(sumaHs)
+          },
+          dy: -10,
+          })),
+          Plot.text(
+            datos_2019,
+            Plot.groupX(
+            { y: 'sum' },
+            {
+            x: 'anio_mision',
+            y: 'mision_hs',
+            fontWeight: 'bold',
+            fontSize: 12,
+            fill: 'black',
+            sort: 'nacionalidad',
+            text: d => {
+            // d: es el grupo de astronautas de todos los países
+            console.log(d)
+            let sumaHs = d3.sum(d, d2 => d2.mision_hs)
+            return d3.format('d')(sumaHs)
+            },
+            dy: -10,
+            })),
       ],
       x: {
         tickFormat: 'd',
@@ -83,7 +114,11 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
       marginBottom: 25,
       height: 420,
       width: 520,
+      
+      
     })
     d3.select('#chart').append(() => chart)
   })
+
+ 
   
